4f44e0270f552eede50ee13e7b11b66e83b2e317
AlexDev404 <alexdev404@users.noreply.github.com>
2022-12-29 18:53:00 -0600

    FMT

--- src/lib/SuperChat.svelte
+++ src/lib/SuperChat.svelte
@@ -1,12 +1,9 @@
 <script>
 	import Sidebar from '$lib/Sidebar.svelte';
-
 </script>
 
 <div class="flex">
 	<Sidebar />
-
 	<slot name="content">slot=content</slot>
-	
-   <slot name="channels">slot=channels</slot>
+	<slot name="channels">slot=channels</slot>
 </div>
