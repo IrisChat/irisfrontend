5ee1bf9051a0fc4d2086cd3c50aa976d034a48c5
AlexDev404 <alexdev404@users.noreply.github.com>
2022-12-31 16:23:06 -0600

    Finish login + register page

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -1,19 +1,53 @@
 <script>
+	import { host } from '$lib/js/config.json';
+	import { toast } from '@zerodevx/svelte-toast';
+
 	let payload = {
 		email: '',
 		username: '',
 		password: ''
 	};
 
+	// @ts-ignore
 	const handleChange = (event) => {
 		Object.defineProperty(payload, event.target.name, {
 			value: event.target.value,
+			// @ts-ignore
 			writeable: true
 		});
 	};
+	// @ts-ignore
 	const handleSubmit = (event) => {
 		event.preventDefault();
 	};
+
+	function Register() {
+		fetch(`${host}/api/v0/auth/register`, {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'POST',
+			body: JSON.stringify(payload)
+		})
+			.then((res) => res.json())
+			.then(function (json) {
+				if (!json.status) {
+					return toast.push(`${json.message}`, {
+						dismissable: false,
+						theme: {
+							'--toastBarBackground': 'red'
+						}
+					});
+				}
+				toast.push('Account Created. Redirecting to login page.');
+				localStorage.setItem('token', json.token);
+				window.location.href = '/auth/login';
+			})
+			.catch(function (res) {
+				console.log(res);
+			});
+	}
 </script>
 
 <main
@@ -24,36 +58,36 @@
 		class="flex flex-wrap items-center justify-center"
 		on:submit={(event) => handleSubmit(event)}
 	>
-	<div class="flex flex-wrap invisiblewrap w-full basis-full items-center justify-center">
-		<div class="smartwrapper flex flex-wrap max-w-fit basis-full items-center justify-center">
-			<div class="text-5xl font-semibold">Register</div>
-			<div
-				class="field email my-4 mx-4 flex max-w-xl basis-full flex-wrap items-center justify-center rounded bg-gray-800 bg-opacity-80 px-2 py-2"
-			>
-				<label for="email" class="basis-full px-2">Email</label>
-				<input
-					class="h-8 w-full rounded-sm bg-gray-800 bg-opacity-20 px-2"
-					type="email"
-					placeholder="Email"
-					name="email"
-					on:change={(event) => handleChange(event)}
-				/>
-			</div>
+		<div class="invisiblewrap flex w-full basis-full flex-wrap items-center justify-center">
+			<div class="smartwrapper flex max-w-fit basis-full flex-wrap items-center justify-center">
+				<div class="text-5xl font-semibold">Register</div>
+				<div
+					class="field email my-4 mx-4 flex max-w-xl basis-full flex-wrap items-center justify-center rounded bg-gray-800 bg-opacity-80 px-2 py-2"
+				>
+					<label for="email" class="basis-full px-2">Email</label>
+					<input
+						class="h-8 w-full rounded-sm bg-gray-800 bg-opacity-20 px-2"
+						type="email"
+						placeholder="Email"
+						name="email"
+						on:change={(event) => handleChange(event)}
+					/>
+				</div>
 
-			<div
-				class="field username my-4 mx-4 flex w-80 max-w-xl basis-full flex-wrap items-center justify-center rounded bg-gray-800 bg-opacity-80 px-2 py-2"
-			>
-				<label for="username" class="basis-full px-2">Username</label>
-				<input
-					class="h-8 w-full rounded-sm bg-gray-800 bg-opacity-20 px-2"
-					type="text"
-					placeholder="Array0x"
-					name="username"
-					on:change={(event) => handleChange(event)}
-				/>
+				<div
+					class="field username my-4 mx-4 flex w-80 max-w-xl basis-full flex-wrap items-center justify-center rounded bg-gray-800 bg-opacity-80 px-2 py-2"
+				>
+					<label for="username" class="basis-full px-2">Username</label>
+					<input
+						class="h-8 w-full rounded-sm bg-gray-800 bg-opacity-20 px-2"
+						type="text"
+						placeholder="Array0x"
+						name="username"
+						on:change={(event) => handleChange(event)}
+					/>
+				</div>
 			</div>
 		</div>
-	</div>
 		<div
 			class="field password my-4 mx-4 flex w-80 max-w-xl basis-full flex-wrap items-center justify-center rounded bg-gray-800 bg-opacity-80 px-2 py-2"
 		>
@@ -68,7 +102,10 @@
 		</div>
 		<div class="footer flex w-full basis-full flex-wrap items-center justify-center text-center">
 			<button
-				class="max-w-xs basis-full rounded-md bg-secondary hover:opacity-80 px-2 py-4"
+				on:click={() => {
+					Register();
+				}}
+				class="max-w-xs basis-full rounded-md bg-secondary px-2 py-4 hover:opacity-80"
 				style="background: #0070e8;">Create Account</button
 			>
 			<div class="my-4 flex basis-full flex-wrap items-center justify-center">
