50150ffcead37ceaf4cc2d71d3387bbf878bb8d2
AlexDev404 <alexdev404@users.noreply.github.com>
2023-01-05 21:51:47 -0600

    Finish conversation search

--- src/lib/ChannelBar/Locator__Conversation.svelte
+++ src/lib/ChannelBar/Locator__Conversation.svelte
@@ -14,7 +14,9 @@
 	import { toast } from '@zerodevx/svelte-toast';
 	//@ts-ignore
 	let conversations: any[] = [];
-
+	let creator: any;
+	let creator_input: any;
+	let search_users: any[] = [];
 	import Conversation from '../Conversation.svelte';
 	onMount(() => {
 		function pollContacts() {
@@ -54,23 +56,110 @@
 				});
 		}
 		pollContacts();
+		// Fit input box to placeholder size
+		creator_input.setAttribute('size', creator_input.getAttribute('placeholder').length);
 		setInterval(() => {
 			pollContacts();
 		}, 15000);
 	});
+
+	function findUsers() {
+		fetch(`${http_host}${API_BASE}user/find/one`, {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'POST',
+			body: JSON.stringify({ username: creator_input.value, id: creator_input.value })
+		})
+			.then(async (res) => {
+				const json = await res.json();
+				search_users = [];
+				search_users.push(json);
+				search_users = search_users; // Trigger
+				// console.log(res);
+			})
+			.catch(function (res) {
+				console.log(res);
+			});
+	}
 </script>
 
 <div
-	class="locator__conversations flex w-full flex-wrap rounded-md py-3 px-2 hover:bg-gray-700 hover:bg-opacity-40 hover:backdrop-blur-sm"
+	class="locator__conversations flex w-full select-none flex-wrap rounded-md py-3 px-2 hover:bg-gray-700 hover:bg-opacity-40 hover:backdrop-blur-sm"
 >
 	<div class="runner flex w-full basis-full">
 		<h5 class="dropdown-header-text dark:text-text">
 			<slot name="header-text">slot=header-text</slot>
 		</h5>
-		<div
-			class="iconHolder my-auto ml-auto rounded-full px-1 py-1 hover:bg-gray-700 hover:bg-opacity-50 hover:backdrop-blur-sm"
-		>
-			<Fa {icon} size="18" />
+
+		<div class="iconHolder relative z-50 flex flex-1 items-center justify-end">
+			<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+			<div
+				class="w-fit rounded-full px-1 py-1 hover:bg-gray-700 hover:bg-opacity-50 hover:backdrop-blur-sm"
+				on:click={creator.classList.toggle('hidden')}
+				on:mouseover={() => creator.classList.add('dismiss_creator')}
+				on:mouseleave={() => creator.classList.remove('dismiss_creator')}
+				on:keydown={creator.classList.toggle('hidden')}
+			>
+				<Fa {icon} size="18" />
+			</div>
+			<span
+				bind:this={creator}
+				class="creator absolute left-36 top-5 m-0 hidden w-fit rounded bg-primary bg-opacity-100 px-4 py-4 text-text"
+			>
+				<div class="creator_wrapper py-4">
+					<div bind:this={creator} class="creator_meta pb-4">
+						<div class="creator_title text-xl font-semibold">Start a Conversation!</div>
+						<div class="creator_description text-sm font-light">
+							Start a conversation with someone you know
+						</div>
+					</div>
+					<form
+						on:submit={(event) => {
+							event.preventDefault();
+						}}
+						action="#"
+					>
+						<input
+							type="text"
+							class="creator_input rounded border-2 border-text bg-transparent px-2 py-2 text-sm font-light text-text"
+							on:keyup={findUsers}
+							bind:this={creator_input}
+							placeholder="Type in an ID and press enter to start"
+						/>
+					</form>
+				</div>
+				<div class="creator_users">
+					{#each search_users as convo, i}
+						<div
+							class="wrap"
+							on:click={() => (window.location.href = `/chat?with=${convo.ID}`)}
+							on:keypress={() => (window.location.href = `/chat?with=${convo.ID}`)}
+						>
+							<Conversation>
+								<svelte:fragment slot="icon">
+									<img
+										class="mt-2 mb-2 w-12 rounded-full bg-gray-700"
+										src={convo.avatar}
+										alt="Profile Avatar"
+									/>
+								</svelte:fragment>
+								<svelte:fragment slot="header-text"
+									>{convo.username}
+									<div class="aboutme text-sm font-light">
+										{#if convo.about.length > 60}
+											{(convo.about = convo.about.substring(0, 30) + '...')}
+										{:else}
+											{convo.about}
+										{/if}
+									</div></svelte:fragment
+								>
+							</Conversation>
+						</div>
+					{/each}
+				</div>
+			</span>
 		</div>
 	</div>
 	<div class="converations basis-full">
@@ -108,3 +197,11 @@
 		</slot>
 	</div>
 </div>
+
+<style>
+	:global(.dismiss_creator) {
+		filter: blur(2px);
+		opacity: 40%;
+		transition: all 150ms ease-in;
+	}
+</style>
