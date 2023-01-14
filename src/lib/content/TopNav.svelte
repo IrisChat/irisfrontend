e2f4cbb9f40fc160184411d594de66e38f00f165
AlexDev404 <alexdev404@users.noreply.github.com>
2023-01-13 19:38:14 -0600

    Logout button

--- src/lib/content/TopNav.svelte
+++ src/lib/content/TopNav.svelte
@@ -7,10 +7,24 @@
 <div
 	class="top-navigation m-0 flex h-16 w-full flex-row items-center bg-opacity-60 text-text shadow-lg"
 >
-	<div class="titlebar flex w-full px-4">
-		<Fa {icon} size="18" class=" mt-1" />
-		<h5 class="title-text">
-			<slot name="title-text">Home</slot>
-		</h5>
+	<div class="titlebar flex w-full justify-around px-4">
+		<div class="pageTitle flex justify-center items-center">
+			<Fa {icon} size="18" />
+			<h5 class="title-text">
+				<slot name="title-text">Home</slot>
+			</h5>
+		</div>
+		<div class="spacer flex-1">&nbsp;</div>
+		<div class="logout">
+			<button
+				class="rounded bg-red-800 px-4 py-2 font-semibold text-white hover:opacity-80"
+				on:click={() => {
+					localStorage.clear();
+					window.location.reload();
+				}}
+			>
+				Logout</button
+			>
+		</div>
 	</div>
 </div>
